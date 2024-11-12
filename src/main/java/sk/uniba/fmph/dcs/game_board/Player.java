3b3c404755e271646ede88d3144650c3b14fc3e2
johankogoodvibes <johnygott@gmail.com>
2024-11-12 23:52:52 +0100

    fixed format issues

--- src/main/java/sk/uniba/fmph/dcs/game_board/Player.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/Player.java
@@ -3,33 +3,8 @@ package sk.uniba.fmph.dcs.game_board;
 import sk.uniba.fmph.dcs.stone_age.InterfacePlayerBoardGameBoard;
 import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
 
-public class Player {
-    private PlayerOrder playerOrder;  // Player's order in the game
-    private InterfacePlayerBoardGameBoard playerBoard;  // Interface for the player's board in the game
-
-    // Constructor
-    public Player(PlayerOrder playerOrder, InterfacePlayerBoardGameBoard playerBoard) {
-        this.playerOrder = playerOrder;
-        this.playerBoard = playerBoard;
-    }
-
-    // Getter for playerOrder
-    public PlayerOrder getPlayerOrder() {
-        return playerOrder;
-    }
-
-    // Setter for playerOrder
-    public void setPlayerOrder(PlayerOrder playerOrder) {
-        this.playerOrder = playerOrder;
-    }
-
-    // Getter for playerBoard
-    public InterfacePlayerBoardGameBoard getPlayerBoard() {
-        return playerBoard;
-    }
-
-    // Setter for playerBoard
-    public void setPlayerBoard(InterfacePlayerBoardGameBoard playerBoard) {
-        this.playerBoard = playerBoard;
-    }
-}
+/**
+ * @param playerOrder Player's order in the game
+ * @param playerBoard Interface for the player's board in the game
+ */
+public record Player(PlayerOrder playerOrder, InterfacePlayerBoardGameBoard playerBoard) { }

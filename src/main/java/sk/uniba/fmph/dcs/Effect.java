a7cca39df9edac28c0c764c58b0581d2e98b4281
johankogoodvibes <johnygott@gmail.com>
2024-11-12 22:51:17 +0100

    Effect enum

---
+++ src/main/java/sk/uniba/fmph/dcs/Effect.java
@@ -0,0 +1,15 @@
+package sk.uniba.fmph.dcs;
+
+public enum Effect {
+    Food,
+    Wood,
+    Clay,
+    Stone,
+    Gold,
+    Tool,
+    Field,
+    Building,
+    OneTimeTool2,
+    OneTimeTool3,
+    OneTimeTool4,
+}

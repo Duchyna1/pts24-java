4d2e6f6f88664ad1f9e0b19d5f4c79ec8b3032bf
Matúš Duchyňa <51196365+Duchyna1@users.noreply.github.com>
2024-11-13 00:20:16 +0100

    Update InterfaceTakeReward.java

--- src/main/java/sk/uniba/fmph/dcs/stone_age/InterfaceTakeReward.java
+++ src/main/java/sk/uniba/fmph/dcs/stone_age/InterfaceTakeReward.java
@@ -2,6 +2,7 @@ package sk.uniba.fmph.dcs.stone_age;
 
 public interface InterfaceTakeReward {
     boolean takeReward(PlayerOrder player, Effect reward);
+    
     HasAction tryMakeAction(PlayerOrder player);
 
 }

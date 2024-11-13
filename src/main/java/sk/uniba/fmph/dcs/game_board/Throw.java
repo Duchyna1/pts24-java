7b2931eb74154eb525dea673ec824b7165940a9a
johankogoodvibes <johnygott@gmail.com>
2024-11-13 18:20:36 +0100

    uvodny commit

---
+++ src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
@@ -0,0 +1,17 @@
+package sk.uniba.fmph.dcs.game_board;
+
+
+public final class Throw {
+    private static final int DICESIDES = 6;
+    private int randomDice() {
+        return (int) (Math.random() * DICESIDES);
+    }
+
+    public int[] hod(final int dices) {
+        int[] res = new int[dices];
+        for (int i = 0; i < dices; i++) {
+            res[i] = randomDice();
+        }
+        return res;
+    }
+}

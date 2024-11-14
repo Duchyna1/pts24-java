e6375eee02bf8a7f20d96f36d5b270505ad265e1
johankogoodvibes <johnygott@gmail.com>
2024-11-14 15:46:24 +0100

    update arbitrary building test

--- src/test/java/sk/uniba/fmph/dcs/game_board/ArbitraryBuildingTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/ArbitraryBuildingTest.java
@@ -15,17 +15,19 @@ public class ArbitraryBuildingTest {
 
         ArbitraryBuilding building = new ArbitraryBuilding(4);
 
-        ArrayList<Effect> wrongResources = new ArrayList<Effect>();
-        wrongResources.add(Effect.WOOD);
-        assertEquals(building.build(wrongResources), OptionalInt.empty());
-
         ArrayList<Effect> goodResources = new ArrayList<Effect>();
+        goodResources.add(Effect.WOOD);
+        assertEquals(building.build(goodResources), OptionalInt.of(3));
+
+        goodResources.clear();
         goodResources.add(Effect.GOLD);
         goodResources.add(Effect.WOOD);
         goodResources.add(Effect.STONE);
         goodResources.add(Effect.CLAY);
         assertEquals(building.build(goodResources), OptionalInt.of(3 + 4 + 5 + 6));
 
+        ArrayList<Effect> wrongResources = new ArrayList<Effect>();
+        wrongResources.add(Effect.WOOD);
         wrongResources.add(Effect.WOOD);
         wrongResources.add(Effect.WOOD);
         wrongResources.add(Effect.WOOD);

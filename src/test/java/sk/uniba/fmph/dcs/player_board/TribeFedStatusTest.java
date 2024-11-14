f69b42e6fa101961df92e8a78c7a38ce1c30242d
filip <filip.sivicek@gmail.com>
2024-11-14 23:04:15 +0100

    TribeFedStatus test

---
+++ src/test/java/sk/uniba/fmph/dcs/player_board/TribeFedStatusTest.java
@@ -0,0 +1,14 @@
+package sk.uniba.fmph.dcs.player_board;
+
+import org.junit.Test;
+
+public class TribeFedStatusTest {
+    @Test
+    public void testAddingFields(){
+        TribeFedStatus tfs = new TribeFedStatus(null, null);
+        for (int i = 0; i < 10; i++){
+            tfs.addField();
+            assert (tfs.)
+        }
+    }
+}
